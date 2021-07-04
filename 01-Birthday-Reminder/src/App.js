23b2c61f16cacf8ff17287faca51c3f753e09c29
TrilochanSahoo <71021905+TrilochanSahoo@users.noreply.github.com>
2021-07-04 19:59:11 +0530

    complete project

--- 01-Birthday-Reminder/src/App.js
+++ 01-Birthday-Reminder/src/App.js
@@ -2,7 +2,22 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
-  return <h2>reminder project setup</h2>;
+  const [dataValue,setDataValue]  = useState(data)
+
+  const clickHandler = ()=>{
+    setDataValue ([])
+  }
+  return (
+    <main>
+      <section className="container">
+        <h3>{dataValue.length} BirthDay available.</h3>
+        <List people={dataValue}></List>
+        <button onClick={clickHandler}>
+          Clear all
+        </button>
+      </section>
+    </main>
+  );
 }
 
 export default App;
