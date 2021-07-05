6e81edbb2b6affb29bd7de4a91bb567d1715b13f
TrilochanSahoo <71021905+TrilochanSahoo@users.noreply.github.com>
2021-07-05 22:50:18 +0530

    update

--- 02-Tour/src/App.js
+++ 02-Tour/src/App.js
@@ -1,11 +1,42 @@
 import React, { useState, useEffect } from 'react'
 import Loading from './Loading'
 import Tours from './Tours'
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
+
 const url = 'https://course-api.com/react-tours-project'
 function App() {
-  return <h2>Tours Project Setup</h2>
+  const [loading,setLoading] = useState(true)
+  const [tours,setTours] = useState([])
+
+  const fetchTourData = async ()=>{
+    setLoading(true)
+    try{
+      const response = await fetch(url)
+      const tour = await response.json() 
+      setLoading(false)
+      setTours(tour)
+    }
+    catch(error){
+      setLoading(false)
+      console.log(error)
+    }
+  }
+  useEffect(()=>{
+    fetchTourData()
+  },[])
+
+  if(loading){
+    return (
+      <main>
+        <Loading className="loading"/>
+      </main>
+    )
+  }
+  
+  return (
+    <main>
+      <Tours places={tours}></Tours>
+    </main>
+  )
 }
 
 export default App
--- 02-Tour/src/Tours.js
+++ 02-Tour/src/Tours.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import Tour from './Tour';
-const Tours = () => {
-  return <h2>tours component</h2>;
+const Tours = ({places}) => {
+  return (
+    <section>
+      <header className="title">
+        <h1>our Tours</h1>
+        <ul className="underline"></ul>
+      </header>
+      <div>
+        {places.map((tour)=>{
+          return <Tour key={tour.id} {...tour}/>
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Tours;
