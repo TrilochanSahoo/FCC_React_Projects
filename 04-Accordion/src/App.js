40d8c470b35b4b4f0e4cb21f8dbe9986f05e4169
TrilochanSahoo <71021905+TrilochanSahoo@users.noreply.github.com>
2021-07-12 20:28:38 +0530

    finishing project

--- 04-Accordion/src/App.js
+++ 04-Accordion/src/App.js
@@ -2,7 +2,17 @@ import React, { useState } from 'react';
 import data from './data';
 import SingleQuestion from './Question';
 function App() {
-  return <h2>accordion project setup</h2>;
+  const [questions,setQuestions] = useState(data)
+  return (
+    <div className="container">
+      <h2>Questions and Answers</h2>
+      <section className="question">
+        {questions.map((Question)=>{
+          return <SingleQuestion key={Question.id} {...Question}></SingleQuestion>
+        })}
+      </section>
+    </div>
+  );
 }
 
 export default App;
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # FCC_React_Projects
+
+#### useState
+
+1. Birthday Reminder
+
+#### useEffect and Conditional Rendering
+
+2. Tours
+3. Reviews
+4. Questions
