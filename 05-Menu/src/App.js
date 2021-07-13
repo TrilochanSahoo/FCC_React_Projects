2c9b819167613de7ca94ba461353160661c91173
TrilochanSahoo <71021905+TrilochanSahoo@users.noreply.github.com>
2021-07-13 19:17:17 +0530

    update : update features

--- 05-Menu/src/App.js
+++ 05-Menu/src/App.js
@@ -3,8 +3,38 @@ import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
 
+const allCategories = ['all', ...new Set(items.map((item)=>{
+  return item.category
+}))]
+
+// console.log(allCategories)
+
 function App() {
-  return <h2>menu project setup</h2>;
+  const [menuItems, setMenuItems] = useState(items)
+  const [categories,setCategories] = useState(allCategories)
+
+  const filterItems = (category)=>{
+    const newItems = items.filter((item)=>{
+      return item.category===category
+    })
+    setMenuItems(newItems)
+
+  }
+
+  return (
+    <menu>
+      <div className="menu section">
+        <header className="title">
+          <h3>Our Menu</h3>
+          <div className="underline"></div>
+        </header>
+        <section>
+          <Categories category={categories} filterItems={filterItems}></Categories>
+          <Menu items={menuItems}></Menu>
+        </section>
+      </div>
+    </menu>
+  )
 }
 
 export default App;
