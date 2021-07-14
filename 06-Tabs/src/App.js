91ebdd7b6abe3eb3c201c5db290665a1245b3ee6
TrilochanSahoo <71021905+TrilochanSahoo@users.noreply.github.com>
2021-07-14 21:39:45 +0530

    update : update feature

--- 06-Tabs/src/App.js
+++ 06-Tabs/src/App.js
@@ -1,10 +1,68 @@
 import React, { useState, useEffect } from 'react'
 import { FaAngleDoubleRight } from 'react-icons/fa'
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
+
+
 const url = 'https://course-api.com/react-tabs-project'
 function App() {
-  return <h2>tabs project setup</h2>
+  const [loading,setLoading] = useState(true)
+  const [jobs,setJobs] = useState([])
+  const [value,setValue] = useState(0)
+
+  const fetchJobData = async()=>{
+    const response = await fetch(url)
+    const newJob = await response.json()
+    setJobs(newJob)
+    setLoading(false)
+  }
+
+  useEffect(()=>{
+    fetchJobData()
+  },[])
+
+
+  if(loading){
+    return (
+      <div className="loading">
+        <h2>Loading...</h2>
+      </div>
+    )
+  }
+
+  const {id, title, dates,duties,company } = jobs[value] 
+  return (
+    <section className="section">
+      <header className="title">
+        <h2>job desc</h2>
+        <div className="underline"></div>
+      </header>
+      <div className="jobs-center">
+        <div className="btn-container">
+          {jobs.map((job,index)=>{
+            return(
+              
+              <button key={index} onClick={()=>{setValue(index)}} className={`job-btn ${index===value && 'active-btn'}`}>{job.company}</button>
+            )
+          })}
+        </div>
+        <article className="job-info">
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p className="job-date">{dates}</p>
+          <p>{duties.map((job,index)=>{
+            return (
+              <div key = {index} className="job-desc">
+                <FaAngleDoubleRight className="job-icon"></FaAngleDoubleRight>
+                <p>{job}</p>
+              </div>
+            )
+          })}</p>
+        </article>
+      </div>
+      <button type="button" className="btn" onClick={()=>{setValue(value+1)}}>
+        more info
+      </button>
+    </section>
+  )
 }
 
 export default App
