48f9bdf388311742337b4e6421ff682949e46846
TrilochanSahoo <71021905+TrilochanSahoo@users.noreply.github.com>
2021-07-15 18:58:56 +0530

    update : adding feature

--- 07-Slider/src/App.js
+++ 07-Slider/src/App.js
@@ -3,7 +3,63 @@ import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
 import { FaQuoteRight } from 'react-icons/fa';
 import data from './data';
 function App() {
-  return <h2>slider project setup</h2>;
+  const [people, setPeople] = useState(data)
+  const [index, setIndex] = useState(1)
+
+  useEffect(()=>{
+    const lastIndex = people.length-1
+    if(index>lastIndex){
+      setIndex(0)
+    }
+    if(index<0){
+      setIndex(lastIndex)
+    }
+  },[index,people])
+
+  useEffect(()=>{
+    const load = setInterval(()=>{
+      setIndex(index+1)
+    },2000)
+
+    return ()=>{
+      clearInterval(load)
+    }
+  },[index])
+
+  return (
+    <section className="section">
+      <header className="title">
+        <h2>
+          <span>/</span>review
+        </h2>
+      </header>
+      <div className="section-center">
+        {people.map((person,personIndex)=>{
+          const {id, image,name, title, quote} = person
+
+          let position = "nextSlide"
+          if(personIndex===index){
+            position = "activeSlide"
+          }
+          if(personIndex===index-1 || (index===0 && personIndex===people.length-1)){
+            position = "lastSlide"
+          }
+
+          return (
+            <article key={id} className={position}>
+              <img src={image} alt={name} className="person-img"></img>
+              <h4>{name}</h4>
+              <h3 className="title">{title}</h3>
+              <p className="text">{quote}</p>
+              <FaQuoteRight className="icon"/>
+            </article>
+          )
+        })}
+        <button className="prev" onClick={()=>{setIndex(index-1)}}>{<FiChevronLeft></FiChevronLeft>}</button>
+        <button className="next" onClick={()=>{setIndex(index+1)}}>{<FiChevronRight></FiChevronRight>}</button>
+      </div>
+    </section>
+  );
 }
 
 export default App;
