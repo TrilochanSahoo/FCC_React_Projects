c8ba3c9f61a207efa4cb632bf2342dd98c8bf9b4
TrilochanSahoo <71021905+TrilochanSahoo@users.noreply.github.com>
2021-07-22 18:31:43 +0530

    feat : adding feature

--- 08-Lorem Ipsum/src/App.js
+++ 08-Lorem Ipsum/src/App.js
@@ -1,11 +1,41 @@
 import React, { useState } from 'react';
 import data from './data';
 function App() {
-  
+  const [count, setCount] = useState(0)
+  const [para , setPara] = useState([])
+
+  const formHandler = (event)=>{
+    event.preventDefault()
+    let amount = parseInt(count)
+    if (count<=0){
+      amount = 1
+    }
+    if(count>8){
+      amount = 8
+    }
+    setPara(data.slice(0,amount))
+  }
   
   return (
-  <h2>lorem ipsum project setup</h2>
-    )
+  <section className="section-center">
+    <h2>
+      Lorem Ipsum generator
+    </h2>
+    <form className="lorem-form" onSubmit={formHandler}>
+      <label htmlFor="amount">Paragraph : </label>
+      <input type="number" name="amount" id="amount" value={count} min="0" onChange={(event)=>setCount(event.target.value)}></input>
+      <button type="submit" className="btn">Generate</button>
+    </form>
+    <article className="result">
+      {count>8 ? <p>Available lorem is 8 .</p>:''}
+      {para.map((item,index)=>{
+        return (
+          <p key={index}>{item}</p>
+        )
+      })}
+    </article>
+  </section>
+  )
 }
 
 export default App;
